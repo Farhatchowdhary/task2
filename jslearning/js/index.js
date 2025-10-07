e2f727db1af22c0fa7e4f39f66f7e7fe6c92a318
Farra <farhatchoudhary2@gmail.com>
2025-10-06 21:59:45 -0400

    To-Do list

---
+++ jslearning/js/index.js
@@ -0,0 +1,11 @@
+const deleteButton = document.getElementById("del-button");
+const completeButton = document.getElementById("com-btn");
+
+deleteButton.addEventListener("click",()=> {
+    alert("Delete button is clicked");
+});
+
+completeButton.addEventListener("click",() => {
+    alert("Complete button is clicked");
+});
+
--- src/components/ProductList.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/ProductList.jsx
-import React from "react";
-
-function ProductList() {
-  return null; // Nothing will render, so no product cards
-}
-
-export default ProductList;
